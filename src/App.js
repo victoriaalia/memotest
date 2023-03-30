c6fdaae4080e5fc951bad5991d9a6c2ff951d479
Victoria Alia <118212418+victoriaalia@users.noreply.github.com>
2023-03-30 20:08:01 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import Board from './components/Board/Board';
+const emojiList = [...'🐤🦋🐧🦭🐼🐭🐷🦄'];
+
+const App = () => {
+  const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([]);
+  const [selectedMemoBlock, setselectedMemoBlock] = useState(null);
+  const [animating, setAnimating] = useState(false);
+
+  useEffect( () => {
+    const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
+    setShuffledMemoBlocks(shuffledEmojiList.map( (emoji, i) => ({ index: i, emoji, flipped: false}) ));
+  }, []);
+
+  const shuffleArray = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
+  const handleMemoClick = memoBlock => {
+    const flippedMemoBlock = { ...memoBlock, flipped: true };
+    let shuffledMemoBlocksCopy = [...shuffledMemoBlocks];
+    shuffledMemoBlocksCopy.splice(memoBlock.index, 1, flippedMemoBlock);
+    setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+    if(selectedMemoBlock === null) {
+      setselectedMemoBlock(memoBlock);
+    } else if(selectedMemoBlock.emoji === memoBlock.emoji) {
+      setselectedMemoBlock(null);
+    } else {
+      setAnimating(true);
+      setTimeout(() => {
+        shuffledMemoBlocksCopy.splice(memoBlock.index, 1, memoBlock);
+        shuffledMemoBlocksCopy.splice(selectedMemoBlock.index, 1, selectedMemoBlock);
+        setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+        setselectedMemoBlock(null);
+        setAnimating(false);
+      }, 1000);
+    }
+  }
+
+  return (
+    <Board memoBlocks={shuffledMemoBlocks} animating={animating}  handleMemoClick={handleMemoClick} />
+  );
+}
+
+export default App;
